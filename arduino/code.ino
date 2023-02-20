3e7de29f689a0511d3f7023177fe84f599936140
Jayson De La Vega <jaysondv0928@gmail.com>
2023-02-20 18:40:18 -0500

    Implemented bluetooth interface

--- arduino/code.ino
+++ arduino/code.ino
@@ -3,16 +3,16 @@
 #include <SoftwareSerial.h>
 
 #define TIMESTEP_MS 1
-#define P_GAIN 0.001
-#define D_GAIN 0.001
+#define P_GAIN 40.0
+#define D_GAIN 0
 
 float th_initial_offset = 0;
 float th_err = 0;
 float th_err_prev = 0;
-float th_desired = 90;
+float th_desired = 0;
 
-int motor_goal_l = 255;
-int motor_goal_r = 255;
+int motor_goal_l = 220;
+int motor_goal_r = 220;
 int motor_cmd_l = 0;
 int motor_cmd_r = 0;
 
@@ -41,16 +41,16 @@ void setup() {
   pinMode(r_motor_pwm, OUTPUT);
   pinMode(r_motor_en_a, OUTPUT);
   pinMode(r_motor_en_b, OUTPUT);
-  pinMode(r_motor_pwm, OUTPUT);
-  pinMode(r_motor_en_a, OUTPUT);
-  pinMode(r_motor_en_b, OUTPUT);
+  pinMode(l_motor_pwm, OUTPUT);
+  pinMode(l_motor_en_a, OUTPUT);
+  pinMode(l_motor_en_b, OUTPUT);
 
-  pinMode(buttonPin, INPUT);
-  
-  digitalWrite(r_motor_en_b, LOW);
-  digitalWrite(r_motor_pwm, LOW);
   digitalWrite(r_motor_en_a, LOW);
   digitalWrite(r_motor_en_b, LOW);
+  digitalWrite(l_motor_en_a, LOW);
+  digitalWrite(l_motor_en_a, LOW);
+
+  pinMode(buttonPin, INPUT);
   
   Serial.begin(9600);
   Wire.begin();
@@ -62,7 +62,7 @@ void setup() {
 
   Serial.println("Calculating offsets, do not move MPU6050");
   delay(1000);
-  mpu.calcOffsets(true, true);
+  // mpu.calcOffsets(true, true);
   Serial.println("Done!\n");
 
   ble.begin(9600);
@@ -72,60 +72,61 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  mpu.update();
-  th_initial_offset = mpu.getAngleZ();
-
-  if (ble.available())
-  {
-    Serial.print("Got the character: "); Serial.println(ble.read());
-    running = true;
-  }
 
   pollForButton();
+  pollForBLE();
+
+  digitalWrite(r_motor_en_a, LOW);
+  digitalWrite(r_motor_en_b, LOW);
+  digitalWrite(l_motor_en_a, LOW);
+  digitalWrite(l_motor_en_b, LOW);
 
   // Main control loop
   while (running)
   {
     unsigned long tStart = millis();
     mpu.update();
-    double current_heading = mpu.getAngleZ();
+    double current_heading = mpu.getAngleZ() - th_initial_offset;
 
-    th_err = (th_desired - th_initial_offset) - current_heading;
+    th_err = (th_desired) - (current_heading - th_initial_offset);
     float C = (P_GAIN * th_err) + (D_GAIN * (th_err - th_err_prev));
     motor_cmd_r = motor_goal_r + C;
     motor_cmd_l = motor_goal_l - C;
 
     th_err_prev = th_err;
 
+    /*
     Serial.print("Heading Z: "); Serial.print((current_heading - th_initial_offset)); Serial.print("\t");
     Serial.print("Right Motor: "); Serial.print(motor_cmd_r); Serial.print("\t");
-    Serial.print("Left Motor Z: "); Serial.println(motor_cmd_l);
-
+    Serial.print("Left Motor: "); Serial.println(motor_cmd_l);
+    */
     if (motor_cmd_r < 0)
-    {
-      digitalWrite(r_motor_en_a, HIGH);
-      digitalWrite(r_motor_en_b, LOW);
-      analogWrite(r_motor_pwm, motor_cmd_r);
-    }
-    else
     {
       digitalWrite(r_motor_en_a, LOW);
       digitalWrite(r_motor_en_b, HIGH);
-      analogWrite(r_motor_pwm, motor_cmd_r);
+      analogWrite(r_motor_pwm, min(255, motor_cmd_r));
+    }
+    else
+    {
+      digitalWrite(r_motor_en_a, HIGH);
+      digitalWrite(r_motor_en_b, LOW);
+      analogWrite(r_motor_pwm, min(255, abs(motor_cmd_r)));
+      
     }
 
     if (motor_cmd_l < 0)
     {
-      digitalWrite(l_motor_en_a, HIGH);
-      digitalWrite(l_motor_en_b, LOW);
-      analogWrite(l_motor_pwm, motor_cmd_r);
+      digitalWrite(l_motor_en_a, LOW);
+      digitalWrite(l_motor_en_b, HIGH);
+      analogWrite(l_motor_pwm, min(255, motor_cmd_l));
     }
     else
     {
-      digitalWrite(l_motor_en_a, LOW);
-      digitalWrite(l_motor_en_b, HIGH);
-      analogWrite(l_motor_pwm, motor_cmd_r);
+      digitalWrite(l_motor_en_a, HIGH);
+      digitalWrite(l_motor_en_b, LOW);
+      analogWrite(l_motor_pwm, min(255, abs(motor_cmd_l)));
     }
+    
     ble.print(mpu.getTemp()); ble.print(", ");
     
     ble.print(mpu.getAccX()); ble.print(", ");
@@ -144,6 +145,7 @@ void loop() {
     ble.println(mpu.getAngleZ());
 
     pollForButton();
+    pollForBLE();
 
     while (millis() - tStart < TIMESTEP_MS) {}
   }
@@ -166,9 +168,29 @@ void pollForButton()
       if (buttonState == HIGH)
       {
         running = !running;
+        Serial.println("Button Pressed!");
       }
     }
   }
 
   lastButtonState = reading;
+}
+
+void pollForBLE()
+{
+  if (ble.available())
+  {
+    byte c = ble.read() - 48;
+    Serial.print("Received: "); Serial.println(c);
+    if (c == 1)
+    {
+      running = true;
+      Serial.println("Starting Loop!");
+    }
+    else if (c==2)
+    {
+      running = false;
+      Serial.println("Ending Loop!");
+    }
+  }
 }
