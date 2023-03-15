cf39f1f63bb54aa0f2efcf52240f8c2a547caef6
Jayson De La Vega <66391744+jdv52@users.noreply.github.com>
2023-03-15 15:16:31 -0400

    Everything works

--- arduino/code.ino
+++ arduino/code.ino
@@ -3,56 +3,167 @@
 #include <SoftwareSerial.h>
 
 #define TIMESTEP_MS 1
-#define P_GAIN 40.0
-#define D_GAIN 0
+#define K_P 40
+#define K_D 0
+
+typedef enum {OFF, CW, CCW} _motor_dir;
+typedef enum {START, DRIVE, TURN, STOP} _tractorFSM_state;
+typedef enum {StableP, PR, RP, StableR} _button_state;
+typedef enum {START_MSG, STOP_MSG, NONE} _ble_msg;
+
+typedef struct
+{
+  int en_a;
+  int en_b;
+  int pwm_pin;
+  _motor_dir dir;
+  int pwm;
+} _Motor;
+
+typedef struct
+{
+  int pin;
+  unsigned long lastDebounceTime;
+  unsigned long debounceDelay;
+  _button_state state;
+  _button_state lastState;
+  bool isTapped;
+} _Button;
+
+typedef struct
+{
+  SoftwareSerial ss;
+  _ble_msg msg;
+} _BLE;
+
+typedef struct
+{
+  int trig;
+  int echo;
+  long duration;
+  int distance;
+} _Ultrasonic;
+
+typedef struct
+{
+  int turn_count;
+  double application_delay_ms;
+  _tractorFSM_state state;
+} _Application;
+
+_Motor r_motor = {10, 9, 11, OFF, 0};
+_Motor l_motor = {8, 7, 6, OFF, 0};
+
+_Button _b = {2, 0, 30, StableR};
+
+SoftwareSerial ss(3, 4);
+_BLE _ble = {ss, NONE};
+
+_Ultrasonic _us = {13, 12, 0, 0};
+
+_Application app;
 
 float th_initial_offset = 0;
-float th_err = 0;
-float th_err_prev = 0;
-float th_desired = 0;
-
-int motor_goal_l = 220;
-int motor_goal_r = 220;
-int motor_cmd_l = 0;
-int motor_cmd_r = 0;
-
-int r_motor_pwm = 11;
-int r_motor_en_a = 10;
-int r_motor_en_b = 9;
-int l_motor_en_a = 8;
-int l_motor_en_b = 7;
-int l_motor_pwm = 6;
-
-int buttonPin = 2;
-int lastButtonState = 0;
-int buttonState = 0;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
-
-int turn_count = 0;
-
-bool running = false;
 
 MPU6050 mpu(Wire);
-SoftwareSerial ble(3, 4);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(r_motor_pwm, OUTPUT);
-  pinMode(r_motor_en_a, OUTPUT);
-  pinMode(r_motor_en_b, OUTPUT);
-  pinMode(l_motor_pwm, OUTPUT);
-  pinMode(l_motor_en_a, OUTPUT);
-  pinMode(l_motor_en_b, OUTPUT);
-
-  digitalWrite(r_motor_en_a, LOW);
-  digitalWrite(r_motor_en_b, LOW);
-  digitalWrite(l_motor_en_a, LOW);
-  digitalWrite(l_motor_en_a, LOW);
-
-  pinMode(buttonPin, INPUT);
-  
   Serial.begin(9600);
+  
+  init_motor(&r_motor);
+  init_motor(&l_motor);
+  
+  init_mpu();
+
+  pinMode(_b.pin, INPUT);
+
+  _ble.ss.begin(9600);
+  Serial.println("Established connection with HM-10 at 9600");
+
+  pinMode(_us.trig, OUTPUT);
+  pinMode(_us.echo, INPUT);
+
+  app = {0, 25, START};
+  
+  Serial.println("Awaiting start...");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long tStart = millis();
+  
+  Application_loop();
+
+  pollForButton(&_b);
+  pollForBLE(&_ble);
+  // ultrasonicPing(&_us);
+  ble_data_tx(&_ble);
+
+  while (millis() - tStart < app.application_delay_ms);
+}
+
+void Application_loop()
+{
+  mpu.update();
+  
+  switch (app.state)
+  {
+    case START:
+      if (_b.isTapped || (_ble.msg == START_MSG))
+      {
+        Serial.println("\tSTART -> DRIVE");
+        // Perform other startup tasks
+        
+        th_initial_offset = mpu.getAngleZ();
+        app.state = DRIVE;
+      }
+      break;
+    case DRIVE:
+      // if ultrasonic sensor distance too close
+      // transition to stop
+      // if detect turning point
+      // transition to turn
+      if (_b.isTapped || (_ble.msg == STOP_MSG))
+      {
+        app.state = STOP;
+        Serial.println("\tDRIVE -> STOP");
+      }
+      else
+      {
+        motor_pd(0);
+      }
+      break;
+    case TURN:
+      break;
+    case STOP:
+      l_motor.dir = OFF;
+      r_motor.dir = OFF;
+      drive_motor(&l_motor);
+      drive_motor(&r_motor);
+
+      Serial.println("Terminating...");
+      Serial.println("\tSTOP -> START");
+
+      // Perform other stop tasks
+
+      app.state = START;
+      break;
+  }
+}
+
+void init_motor(_Motor* m)
+{
+  pinMode(m->en_a, OUTPUT);
+  pinMode(m->en_b, OUTPUT);
+  pinMode(m->pwm_pin, OUTPUT);
+
+  digitalWrite(m->en_a, LOW);
+  digitalWrite(m->en_b, LOW);
+}
+
+void init_mpu()
+{
   Wire.begin();
 
   byte status = mpu.begin();
@@ -62,135 +173,167 @@ void setup() {
 
   Serial.println("Calculating offsets, do not move MPU6050");
   delay(1000);
-  // mpu.calcOffsets(true, true);
+  mpu.calcOffsets(true, true);
   Serial.println("Done!\n");
-
-  ble.begin(9600);
-  Serial.println("Established connection with HM-10 at 9600");
-  Serial.println("Awaiting start...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  pollForButton();
-  pollForBLE();
-
-  digitalWrite(r_motor_en_a, LOW);
-  digitalWrite(r_motor_en_b, LOW);
-  digitalWrite(l_motor_en_a, LOW);
-  digitalWrite(l_motor_en_b, LOW);
-
-  // Main control loop
-  while (running)
-  {
-    unsigned long tStart = millis();
-    mpu.update();
-    double current_heading = mpu.getAngleZ() - th_initial_offset;
-
-    th_err = (th_desired) - (current_heading - th_initial_offset);
-    float C = (P_GAIN * th_err) + (D_GAIN * (th_err - th_err_prev));
-    motor_cmd_r = motor_goal_r + C;
-    motor_cmd_l = motor_goal_l - C;
-
-    th_err_prev = th_err;
-
-    /*
-    Serial.print("Heading Z: "); Serial.print((current_heading - th_initial_offset)); Serial.print("\t");
-    Serial.print("Right Motor: "); Serial.print(motor_cmd_r); Serial.print("\t");
-    Serial.print("Left Motor: "); Serial.println(motor_cmd_l);
-    */
-    if (motor_cmd_r < 0)
-    {
-      digitalWrite(r_motor_en_a, LOW);
-      digitalWrite(r_motor_en_b, HIGH);
-      analogWrite(r_motor_pwm, min(255, motor_cmd_r));
-    }
-    else
-    {
-      digitalWrite(r_motor_en_a, HIGH);
-      digitalWrite(r_motor_en_b, LOW);
-      analogWrite(r_motor_pwm, min(255, abs(motor_cmd_r)));
-      
-    }
-
-    if (motor_cmd_l < 0)
-    {
-      digitalWrite(l_motor_en_a, LOW);
-      digitalWrite(l_motor_en_b, HIGH);
-      analogWrite(l_motor_pwm, min(255, motor_cmd_l));
-    }
-    else
-    {
-      digitalWrite(l_motor_en_a, HIGH);
-      digitalWrite(l_motor_en_b, LOW);
-      analogWrite(l_motor_pwm, min(255, abs(motor_cmd_l)));
-    }
-    
-    ble.print(mpu.getTemp()); ble.print(", ");
-    
-    ble.print(mpu.getAccX()); ble.print(", ");
-    ble.print(mpu.getAccY()); ble.print(", ");
-    ble.print(mpu.getAccZ()); ble.print(", ");
-
-    ble.print(mpu.getGyroX()); ble.print(", ");
-    ble.print(mpu.getGyroY()); ble.print(", ");
-    ble.print(mpu.getGyroZ()); ble.print(", ");
-
-    ble.print(mpu.getAccAngleX()); ble.print(", ");
-    ble.print(mpu.getAccAngleY()); ble.print(", ");
-
-    ble.print(mpu.getAngleX()); ble.print(", ");
-    ble.print(mpu.getAngleY()); ble.print(", ");
-    ble.println(mpu.getAngleZ());
-
-    pollForButton();
-    pollForBLE();
-
-    while (millis() - tStart < TIMESTEP_MS) {}
-  }
-}
-
-void pollForButton()
+void pollForButton(_Button* b)
 {
-  int reading = digitalRead(buttonPin);
-  if (reading != lastButtonState)
-  {
-    lastDebounceTime = millis();
-  }
+  int reading = digitalRead(b->pin);
 
-  if ((millis() - lastDebounceTime) > debounceDelay)
-  {
-    if (reading != buttonState)
-    {
-      buttonState = reading;
+  bool released = true;
+  b->lastState = b->state;
 
-      if (buttonState == HIGH)
+  switch (b->state)
+  {
+    case StableR:
+      if (reading)
       {
-        running = !running;
-        Serial.println("Button Pressed!");
+        b->lastDebounceTime = millis();
+        b->state = RP;
       }
-    }
+      break;
+    case StableP:
+      if (!reading)
+      {
+        b->lastDebounceTime = millis();
+        b->state = PR;
+      }
+      break;
+    case RP:
+      if (!reading)
+      {
+        b->state = StableR;
+      }
+      else if ((millis() - b->lastDebounceTime) > b->debounceDelay)
+      {
+        b->state = StableP;
+      }
+      break;
+    case PR:
+      if (reading)
+      {
+        b->state = StableP;
+      }
+      else if ((millis() - b->lastDebounceTime) > b->debounceDelay)
+      {
+        b->state = StableR;
+      }
+      break;
   }
-
-  lastButtonState = reading;
+  
+  if ((b->lastState != b->state) && b->state == StableP)
+  {
+    b->isTapped = true;
+  }
+  else
+    b->isTapped = false;
 }
 
-void pollForBLE()
+void pollForBLE(_BLE* ble)
 {
-  if (ble.available())
+  if (ble->ss.available())
   {
-    byte c = ble.read() - 48;
+    byte c = ble->ss.read() - 48;
     Serial.print("Received: "); Serial.println(c);
-    if (c == 1)
+    switch(c)
     {
-      running = true;
-      Serial.println("Starting Loop!");
-    }
-    else if (c==2)
-    {
-      running = false;
-      Serial.println("Ending Loop!");
+      case 1:
+        Serial.println("Starting Loop!");
+        ble->msg = START_MSG;
+        break;
+      case 2:
+        Serial.println("Ending Loop!");
+        ble->msg = STOP_MSG;
+        break;
+      default:
+        ble->msg = NONE;
     }
   }
+}
+
+void ultrasonicPing(_Ultrasonic* u)
+{
+  digitalWrite(u->trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(u->trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(u->trig, LOW);
+  u->duration = pulseIn(u->echo, HIGH);
+
+  u->distance = u->duration * 0.034 / 2;
+}
+
+void motor_pd(float th_goal)
+{
+  static float th_err_prev = 0;
+  
+  float current_heading = mpu.getAngleZ();
+
+  float th_err = (th_goal) - (current_heading - th_initial_offset);
+  float PD = (K_P * th_err) + (K_D * (th_err - th_err_prev));
+  int motor_cmd_r = 220 + PD;
+  int motor_cmd_l = 220 - PD;
+
+  Serial.print(th_err); Serial.print("\t");
+  Serial.print(motor_cmd_r); Serial.print("\t");
+  Serial.println(motor_cmd_l);
+
+  th_err_prev = th_err;
+
+  if (motor_cmd_r < 0)
+  {
+    r_motor.dir = CW;
+  }
+  else if (motor_cmd_r > 0)
+  {
+    r_motor.dir = CCW;
+  }
+  else
+    r_motor.dir = OFF;
+
+  if (motor_cmd_l < 0)
+  {
+    l_motor.dir = CW;
+  }
+  else if (motor_cmd_l > 0)
+  {
+    l_motor.dir = CCW;
+  }
+  else
+    l_motor.dir = OFF;
+  
+  r_motor.pwm = abs(motor_cmd_r);
+  l_motor.pwm = abs(motor_cmd_l);
+
+  drive_motor(&l_motor);
+  drive_motor(&r_motor);
+}
+
+void drive_motor(_Motor* m)
+{
+  switch (m->dir)
+  {
+    case CW:
+      digitalWrite(m->en_a, LOW);
+      digitalWrite(m->en_b, HIGH);
+      analogWrite(m->pwm_pin, min(255, m->pwm));
+      break;
+    case CCW:
+      digitalWrite(m->en_a, HIGH);
+      digitalWrite(m->en_b, LOW);
+      analogWrite(m->pwm_pin, min(255, m->pwm));
+      break;
+    case OFF:
+      digitalWrite(m->en_a, LOW);
+      digitalWrite(m->en_b, LOW);
+      break;
+  }
+}
+
+void ble_data_tx(_BLE* ble)
+{
+  ble->ss.print("WHAT IS UP MY NEIGHBOR");
+  ble->ss.print("MSG 2");
+  ble->ss.print("MSG 3");
 }
